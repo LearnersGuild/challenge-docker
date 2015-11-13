302552f68baa771a04955a38ae4a4883086ecd3a
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2015-11-12 16:48:09 -0800

    better environment variable configuration

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-floomatic --no-browser --share https://floobits.com/jeffreywescott/foo . > /dev/null 2>&1 &
+function die {
+  exitcode=${$2:-1}
+  echo "$1" && exit $exitcode
+}
+
+test $FLOOBITS_USER || die "FLOOBITS_USER environment variable must be set"
+test $FLOOBITS_WORKSPACE || die "FLOOBITS_WORKSPACE environment variable must be set"
+
+FLOOBITS_URL="https://floobits.com/${FLOOBITS_USER}/${FLOOBITS_WORKSPACE}"
 export SHELL=/bin/bash
-tmux new-session -n foo 'flootty --unsafe --url https://floobits.com/jeffreywescott/foo --create terminal1'
+
+floomatic --no-browser --share ${FLOOBITS_URL} . > /dev/null 2>&1 &
+tmux new-session -n foo "flootty --unsafe --url ${FLOOBITS_URL} --create terminal1"
